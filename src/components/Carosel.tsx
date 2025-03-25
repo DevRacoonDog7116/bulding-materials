291af486553ee439ec41ed56499fe77ac8bdf52a
S Future <tiaguitov08@gmail.com>
2025-03-25 20:55:02 +0900

    Update Carosel.tsx

--- src/components/Carosel.tsx
+++ src/components/Carosel.tsx
@@ -6,9 +6,9 @@ const images = [
   '/images/img1.jpg',
   '/images/img2.jpg',
   '/images/img3.jpg',
-  '/images/img4.jpg',
-  '/images/img5.jpg',
-  '/images/img6.jpg',
+  '/images/img4.jpg', 
+  '/images/img5.jpg', 
+  '/images/img6.jpg', 
 ];
 
 const Carousel: React.FC = () => {
